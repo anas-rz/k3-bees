9e01500969697f89faf1ec3b021d2ae171b4cdaf
Muhammad Anas Raza <memanasraza@gmail.com>
2024-03-24 22:49:13 -0400

    initial commit - add ia3

---
+++ k3_peft/layers/ia3/layer.py
@@ -0,0 +1,53 @@
+from keras import layers, ops
+
+from k3_peft.api_export import k3_export
+
+
+@k3_export('k3_peft.tuners.IA3')
+class IA3(layers.Layer):
+    def __init__(
+        self, 
+        in_layer, 
+        multiplier=1.0, 
+        pre=False, 
+        **kwargs
+    ):
+        super().__init__()
+        
+        self.shape = ops.shape(in_layer.kernel)
+        if in_layer.__class__.__name__ == 'Conv2D':
+            in_dim = self.shape[2]
+            out_dim = in_layer.filters
+            if pre:
+                train_dim = in_dim
+            else:
+                train_dim = out_dim
+            self.weight = self.add_weight((1, 1, 1, train_dim), initializer='zeros')
+        else:
+            in_dim = self.shape[0]
+            out_dim = in_layer.units
+            if pre:
+                train_dim = in_dim
+            else:
+                train_dim = out_dim
+            
+            self.weight = self.add_weight((train_dim,), initializer='zeros')
+
+        self.multiplier = multiplier
+        self.in_call = None
+        self.in_layers = [in_layer]
+        self.pre = pre
+
+    def apply_to(self):
+        self.in_call = self.in_layers[0].call
+        self.in_layers[0].call = self.call
+
+    def call(self, x):
+        if self.pre:
+            x = x * (1 + self.weight * self.multiplier)
+        out = self.in_call(x)
+        dtype = out.dtype
+        if not self.pre:
+            out = out * (1 + self.weight * self.multiplier)
+            out = ops.cast(out, dtype)
+        return out
---
+++ k3_peft/layers/ia3/layer_test.py
@@ -0,0 +1,29 @@
+
+import pytest
+import keras
+
+from keras import layers, ops
+from k3_peft.layers.ia3.layer import IA3
+
+@pytest.mark.parametrize(
+    "in_layer, multiplier, pre, in_features, out_features",
+    [
+        ('dense', 1.0, False, 32, 56),
+        ('dense', 1.0, True, 32, 56),
+        ('conv2d', 2.0, False, 32, 56),
+        ('conv2d', 2.0, True, 32, 56),
+    ]
+)
+def test_ia3(in_layer, multiplier, pre, in_features, out_features):
+    if in_layer == 'dense':
+        in_layer = layers.Dense(out_features)
+        input_shape = (10, in_features)
+    elif in_layer == 'conv2d':
+        in_layer = layers.Conv2D(out_features, 3)
+        input_shape = (3, 12, 12, in_features)
+    inputs = keras.random.uniform((input_shape))
+    expected_shape = ops.shape(in_layer(inputs))
+    ia3 = IA3(in_layer, multiplier, pre)
+    ia3.apply_to()
+    output = ia3(inputs)
+    assert ops.shape(output) == expected_shape
